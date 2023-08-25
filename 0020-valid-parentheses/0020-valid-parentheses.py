d6b3c6a666d26a3d122c37f16f87ce6f0a11c03c
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-08-26 00:04:33 +0700

    Time: 41 ms (65.92%), Space: 16.4 MB (51.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,22 +1,20 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) == 1:
-            return False
-        
         parenthesesStack = []
-        closeToOpen = {"}" : "{", ")" : "(", "]" : "["}
+        closeToOpen = {"}" : "{", "]" : "[", ")" : "("}
         
         for symbol in s:
             if symbol in closeToOpen:
-                if parenthesesStack and parenthesesStack[-1]  == closeToOpen[symbol]:
+                if parenthesesStack and parenthesesStack[-1] == closeToOpen[symbol]:
                     parenthesesStack.pop()
                 else:
                     return False
             else:
                 parenthesesStack.append(symbol)
         
-        if len(parenthesesStack) > 0:
-            return False 
+        if parenthesesStack:
+            return False
         else:
             return True
+            
         
