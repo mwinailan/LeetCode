8b513620598ee74c7e52c29cf21d839f4cc56569
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-08-26 17:26:42 +0700

    Time: 119 ms (32.93%), Space: 22.4 MB (8.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groupedStrings = defaultdict(list)
         for s in strs:
-            countOfLetters = [0] * 26
+            countOfLetters = [0 for i in range(26 + 1)]
             
             for c in s:
                 countOfLetters[ord(c) - ord("a")] += 1
