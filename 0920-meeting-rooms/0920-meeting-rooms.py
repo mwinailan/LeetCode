c27c2d27bcb62e189d17fc8b1dc5ebabdf97cce7
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-09-04 16:54:09 -0700

    Create 0920-meeting-rooms.py

---
+++ 0920-meeting-rooms/0920-meeting-rooms.py
@@ -0,0 +1,22 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
+        # Write your code here
+        intervals.sort(key = lambda i : intervals.start)
+
+        for i in range(1, len(intervals)):
+            if intervals[i-1].end > intervals[i].start:
+                return False
+        
+        return True
