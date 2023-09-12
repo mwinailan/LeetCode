7c37cfe7d01eb926800cf5373e08a56bc8d7f6f5
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-09-11 17:04:11 -0700

    Time: 1687 ms (39.08%), Space: 16.5 MB (43.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,25 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longestSubstringLength = 0
-        longestPalindromeSubstring = ""
+        longestSubstring = 0
+        res = ""
         
         for i in range(len(s)):
             left, right = i, i
-            
-            while (left >= 0 and right < len(s) and s[left] == s[right]):
-                if (right - left + 1 > longestSubstringLength):
-                    longestPalindromeSubstring = s[left:right+1]
-                    longestSubstringLength = right - left + 1
-                right += 1
+            while left in range(len(s)) and right in range(len(s)) and s[left] == s[right]:
+                longestSubstring = max(longestSubstring, right - left + 1)
+                if (right - left + 1 == longestSubstring):
+                    res = s[left:right + 1]
                 left -= 1
-                
-        for i in range(len(s)):
+                right += 1
+            
             left, right = i, i + 1
-            
-            while (left >= 0 and right < len(s) and s[left] == s[right]):
-                if (right - left + 1 > longestSubstringLength):
-                    longestPalindromeSubstring = s[left:right+1]
-                    longestSubstringLength = right - left + 1
-                right += 1
+            while left in range(len(s)) and right in range(len(s)) and s[left] == s[right]:
+                longestSubstring = max(longestSubstring, right - left + 1)
+                if (right - left + 1 == longestSubstring):
+                    res = s[left:right + 1]
                 left -= 1
-                
-            
+                right += 1
         
-        return longestPalindromeSubstring
-                
+        
+        return res
+        
