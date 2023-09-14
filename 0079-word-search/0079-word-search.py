6ebc94158910a5fd5b130f8c9b219632d9876b13
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-09-14 16:51:41 -0700

    Time: 9112 ms (5.54%), Space: 16.4 MB (70.12%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -3,20 +3,22 @@ class Solution:
         rows, cols = len(board), len(board[0])
         isVisited = set()
         
-        def findWord(row, col, indexOfWord):
-            if indexOfWord == len(word):
+        def wordCheck(r, c, index):
+            if index == len(word):
                 return True
-            if row not in range(rows) or col not in range(cols) or board[row][col] != word[indexOfWord] or (row,col) in isVisited:
+            
+            if r not in range(rows) or c not in range(cols) or board[r][c] != word[index] or (r,c) in isVisited:
                 return False
             
-            isVisited.add((row,col))
-            result = findWord(row + 1, col, indexOfWord + 1) or findWord(row - 1, col, indexOfWord + 1) or findWord(row, col + 1, indexOfWord + 1) or findWord(row, col - 1, indexOfWord + 1)
-            isVisited.remove((row,col))
-            return result
+            isVisited.add((r,c))
+            res = wordCheck(r + 1, c, index + 1) or wordCheck(r - 1, c, index + 1) or wordCheck(r, c + 1, index + 1) or wordCheck(r, c - 1, index + 1)
+            isVisited.remove((r,c))   
+            return res
+        
         
         for r in range(rows):
             for c in range(cols):
-                if findWord(r, c, 0) == True:
+                if wordCheck(r, c, 0):
                     return True
         
         return False
