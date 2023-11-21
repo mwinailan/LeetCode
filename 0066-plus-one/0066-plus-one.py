5f12e0958e349dc3a49bb9bf3d02c16f9ac7c501
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-11-20 21:04:54 -0800

    Time: 49 ms (6.96%), Space: 16.3 MB (35.19%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -3,16 +3,23 @@ class Solution:
         digits = digits[::-1]
         carry = 1
         i = 0
+        
         while carry:
+            # Case 1 : if index in-bounds
             if i < len(digits):
+                # Case 1a: if digits[i] is 9, then we keep the carry as is
                 if digits[i] == 9:
                     digits[i] = 0
-                    i += 1
-                else:
-                    carry = 0
+                # Case 1b: if digits[i] != 9, then carry = 0
+                else :
                     digits[i] += 1
+                    carry = 0
+            
+            # Case 2 : if index out of bounds
             else:
-                carry = 0
                 digits.append(1)
+                carry = 0
+            
+            i += 1
         
         return digits[::-1]
