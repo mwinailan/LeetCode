81f2e9afb931b04c1212f005c99b6f0fa8c21d54
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-12-08 21:10:13 -0800

    Update 0015-3sum.py

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        if nums is None:
+            return None
         nums.sort()
-        for i, num in enumerate(nums):
-            if num > 0:
-                break
-            if i > 0 and num == nums[i-1]:
+        
+        threeSumResult = []
+        
+        for i in range(len(nums) - 2):
+            j, k = i + 1, len(nums) - 1
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            l, r = i + 1, len(nums) - 1
-            while (l < r):
-                if num + nums[l] + nums[r] < 0:
-                    l += 1
-                elif num + nums[l] + nums[r] > 0:
-                    r -= 1
-                else:
-                    res.append([num, nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
-        
-        return res
                 
+            # Find j and k with a 2 pointer solution
+            while j < k:
+                totalSum = nums[i] + nums[j] + nums[k]
+                # Case 1: totalsum is larger than 0, move k by 1 to the left
+                if totalSum > 0:
+                    k -= 1
+                # Case 2: totalsum is smaller than 0, move j by 1 to the right
+                elif totalSum < 0:
+                    j += 1
+                # Case 3: totalsum is 0, add to the threeSumResult, and move j pointer
+                else:        
+                    threeSumResult.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    k -= 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
         
+        return threeSumResult
