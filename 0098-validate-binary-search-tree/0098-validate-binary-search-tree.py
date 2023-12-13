dbb3ceb5c503ca23add69e57b9caa451d39d5058
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-12-12 22:47:04 -0800

    Time: 43 ms (85.84%), Space: 18.8 MB (62.72%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,13 +6,14 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def isValid(node, leftBound, rightBound):
+        # DFS Solution: Check if root is valid using left and right boundaries
+        def isValid(node, leftBoundary, rightBoundary):
             if not node:
                 return True
-            if not(node.val < rightBound and node.val > leftBound):
+            if not (node.val < rightBoundary and node.val > leftBoundary):
                 return False
             
-            return isValid(node.left, leftBound, node.val) and isValid(node.right, node.val, rightBound)
-            
+            return isValid(node.left, leftBoundary, node.val) and isValid(node.right, node.val, rightBoundary)
+        
         return isValid(root, float("-Inf"), float("Inf"))
         
