5b59c1f20b84f2937057c759333ae664e9ee9332
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-12-12 23:00:06 -0800

    Time: 44 ms (91.98%), Space: 16.8 MB (74.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,17 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        sCharCount = Counter(s)
-        tCharCount = Counter(t)
+        #sCharCount = Counter(s)
+        #tCharCount = Counter(t)
+        
+        if len(s) != len(t):
+            return False
+        
+        sCharCount = [0] * 26
+        tCharCount = [0] * 26
+        
+        for i in range(len(s)):
+            sCharCount[ord(s[i]) - ord("a")] += 1
+            tCharCount[ord(t[i]) - ord("a")] += 1
         
         return sCharCount == tCharCount
         
