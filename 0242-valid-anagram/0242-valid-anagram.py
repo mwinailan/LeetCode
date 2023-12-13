ec6fb1ed7d093cfea0285fc6edc911c2080fa63d
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-12-12 22:58:09 -0800

    Time: 46 ms (88.20%), Space: 16.8 MB (74.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (len(s) != len(t)):
-            return False
+        sCharCount = Counter(s)
+        tCharCount = Counter(t)
         
-        letterCountS = {}
-        letterCountT = {}
-
-        for letter in s:
-            letterCountS[letter] = 1 + letterCountS.get(letter, 0)
-        
-        for letter in t:
-            letterCountT[letter] = 1 + letterCountT.get(letter, 0)
-
-        if (letterCountS == letterCountT):
-            return True
-        else:
-            return False
-
-    
+        return sCharCount == tCharCount
         
