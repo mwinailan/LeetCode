759cb4bf9d0cf43fca81c0ef4ef715ef8432bcba
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-12-13 22:48:44 -0800

    Time: 591 ms (74.80%), Space: 29.4 MB (29.02%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -2,19 +2,18 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
-        maxWater = 0
+        maxArea = 0
         
-        
-        #2 pointer solution
-        while (left < right):
-            currentWaterArea = (right - left) * min(height[left], height[right])
-            maxWater = max(maxWater, currentWaterArea)
+        # 2 pointer greedy solution
+        while (left <= right):
+            currentArea = (right - left) * min(height[left], height[right])
+            maxArea = max(maxArea, currentArea)
             
-            # Case 1: if left height is lower move the left pointer
-            if height[left] < height[right]:
-                left += 1
-            # Case 1: if right height is lower move the right pointer
-            else:
+            # Case 1: if left height is taller, shift the right pointer
+            if height[left] > height[right]:
                 right -= 1
-        
-        return maxWater
+            # Case 2: if right height is taller, shift the left pointer
+            else:
+                left += 1
+                
+        return maxArea
