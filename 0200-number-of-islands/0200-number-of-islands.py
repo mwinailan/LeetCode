7a3adccf3251da077a2a627e65e35228e9814e12
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2023-12-28 22:50:55 -0800

    Update 0200-number-of-islands.py

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,35 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+
+        ROW, COLS = len(grid), len(grid[0])
         isVisited = set()
-        
-        
-        # Use DFS to mark an island starting from coordinates (i, j)
-        def markIslands(i, j):
-            # TODO: Stopping Criterias
-            if i not in range(ROWS) or j not in range(COLS) or (i,j) in isVisited or grid[i][j] == "0":
-                return
-            
-            isVisited.add((i,j))
-            
-            markIslands(i + 1, j)
-            markIslands(i - 1, j)
-            markIslands(i, j + 1)
-            markIslands(i, j - 1)
-        
-        numIslands = 0
-        # Loop through every point in the grid, and run the DFS function if it meets criterias
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == "1" and (i,j) not in isVisited:
-                    markIslands(i, j)
-                    numIslands += 1
-        
-        return numIslands
+
+        # DFS solution to mark all islands
+        def markIslands(row, col):
+
+                isVisited.add((row,col))
+                stack = []
+                stack.append([row,col])
+
+                while stack:
+                    cRow, cCol = stack.pop()
                     
+                    directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                    for dx, dy in directions:
+                        nRow = cRow + dx
+                        nCol = cCol + dy
+                        if nRow not in range(ROW) or nCol not in range(COLS) or (nRow,nCol) in isVisited or grid[nRow][nCol] == "0":
+                            continue
+                        else:
+                            stack.append([nRow, nCol])
+                            isVisited.add((nRow,nCol))
+
+        numIslands = 0
+        # Loop through every grid position and call DFS function        
+        for r in range(ROW):
+            for c in range(COLS):
+                if (r,c) not in isVisited and grid[r][c] == "1":
+                    markIslands(r, c)
+                    numIslands += 1
+
+        return numIslands
