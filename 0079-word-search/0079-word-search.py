7791f1f015ead74de7d5fc5b319130da16c26a4f
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-01-03 17:51:31 -0800

    Time: 7294 ms (19.31%), Space: 17.5 MB (7.80%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,24 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS = len(board), len(board[0])
-        isVisitedPath = set()
+        ROW = len(board)
+        COL = len(board[0])
         
-        # DFS appreoach to visit grid tiles
-        def findWord(r, c, i):
-            #TODO: stopping criteria
+        #DFS solution to find words
+        def wordExists(r, c, isVisited, i):
             if i == len(word):
                 return True
-            if r not in range(ROWS) or c not in range(COLS) or (r,c) in isVisitedPath or board[r][c] != word[i]:
+            if r not in range(ROW) or c not in range(COL) or (r,c) in isVisited or board[r][c] != word[i]:
                 return False
             
-            isVisitedPath.add((r,c))
-            res = findWord(r + 1, c, i + 1) or findWord(r - 1, c, i + 1) or findWord(r, c + 1, i + 1) or findWord(r, c - 1, i + 1)
-            isVisitedPath.remove((r,c))
-            
+            isVisited.add((r,c))
+            res = wordExists(r + 1, c, isVisited, i + 1) or wordExists(r - 1, c, isVisited, i + 1) or wordExists(r, c + 1, isVisited, i + 1) or wordExists(r, c - 1, isVisited, i + 1)
+            isVisited.remove((r,c))
             return res
         
-        # Iterate through every tile
-        for r in range(ROWS):
-            for c in range(COLS):
-                if findWord(r, c, 0):
+        # Iterate through every cell in the grid
+        for r in range(ROW):
+            for c in range(COL):
+                if wordExists(r, c, set(), 0):
                     return True
         
         return False
