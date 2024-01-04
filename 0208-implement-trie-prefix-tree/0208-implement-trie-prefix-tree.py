fe976f1a1242457dd1e4668ec7e425dd66ce2376
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-01-03 17:24:27 -0800

    Time: 109 ms (96.62%), Space: 31.9 MB (71.59%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,4 +1,4 @@
-class MapNode:
+class TrieNode:
     def __init__(self):
         self.children = {}
         self.endOfWord = False
@@ -6,24 +6,21 @@ class MapNode:
 class Trie:
 
     def __init__(self):
-        self.root = MapNode()
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
         current = self.root
-        
         for c in word:
-            # Case 1 : c not a child of the previous letter
             if c not in current.children:
-                current.children[c] = MapNode()
+                current.children[c] = TrieNode()
             current = current.children[c]
         
         current.endOfWord = True
         
+
     def search(self, word: str) -> bool:
         current = self.root
-        
         for c in word:
-            # Case 1 : c not a child of the previous letter
             if c not in current.children:
                 return False
             current = current.children[c]
@@ -33,14 +30,13 @@ class Trie:
 
     def startsWith(self, prefix: str) -> bool:
         current = self.root
-        
         for c in prefix:
-            # Case 1 : c not a child of the previous letter
             if c not in current.children:
                 return False
             current = current.children[c]
         
         return True
+        
 
 
 # Your Trie object will be instantiated and called as such:
