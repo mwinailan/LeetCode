4b4f989d15a0d1aac6ab7bcbe7cd361044867964
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-01-04 16:50:25 -0800

    Time: 85 ms (95.85%), Space: 23 MB (5.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countOfNums = [[] for i in range(len(nums) + 1)]
-        letterCounts = {}
-        result = []
+        # Have the count of each num as an index
+        frequency = [[] for i in range(len(nums) + 1)]
+        letterCount = {}
         
-        for n in nums:
-            letterCounts[n] = 1 + letterCounts.get(n, 0)
+        # Count each num in nums
+        for num in nums:
+            letterCount[num] = 1 + letterCount.get(num, 0)
         
-        for n, count in letterCounts.items():
-            print(n, count)
-            countOfNums[count].append(n)
-    
-        for i in range(len(nums), 0, -1):
-            for element in countOfNums[i]:
-                result.append(element)
-                if len(result) == k:
-                    return result
+        # Put the letter counts into the frequency array
+        for num, count in letterCount.items():
+            frequency[count].append(num)
+        
+        # Get the top k frequent elements
+        topKFrequent = []
+        for i in range(len(nums), -1, -1):
+            for n in frequency[i]:
+                topKFrequent.append(n)
+                
+                if len(topKFrequent) == k:
+                    return topKFrequent
             
+        
