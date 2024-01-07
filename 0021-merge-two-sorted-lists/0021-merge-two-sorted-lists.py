86c8c801c67c7639280e70138476fdfd1d4e00d0
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-01-06 23:58:48 -0800

    Time: 34 ms (94.56%), Space: 17.3 MB (23.30%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -9,18 +9,22 @@ class Solution:
         current = dummy
         
         while list1 and list2:
+            # Case 1:if list1 value is smaller than list2, add list1 to current node
             if list1.val < list2.val:
                 current.next = list1
                 list1 = list1.next
+            
+            # Case 2:if list2 value is smaller or equal than list1, add list2 to current node
             else:
                 current.next = list2
                 list2 = list2.next
             current = current.next
         
+        # If there are leftover elements, join them all to current node
         if list1:
             current.next = list1
         if list2:
             current.next = list2
+            
         
         return dummy.next
-        
