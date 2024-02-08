a6cffaac03d354cc90fb12a989737ac04a330c35
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-02-07 20:24:36 -0800

    Time: 66 ms (54.03%), Space: 17.8 MB (66.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Hashmap solution
-        valueToIndex = {}
+        numsToDiff = {}
         
-        # Iterate through nums to find the solution O(n)
-        for i, num in enumerate(nums):
-            difference = target - num
-            if difference in valueToIndex:
-                return [i, valueToIndex[difference]]
+        for i, n in enumerate(nums):
+            diff =  target - n
             
-            valueToIndex[num] = i
-                
+            if diff in numsToDiff:
+                return [i, numsToDiff[diff]]
+            
+            numsToDiff[n] = i
+    
+        
