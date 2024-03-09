8a9731de8c9effa1ec5ff2542fe888fe1fa04f0b
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-03-08 18:54:22 -0800

    Time: 1186 ms (41.69%), Space: 16.6 MB (59.82%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longestPalindromeLength = 0
-        currentSubstring = ""
-        # Loop through every letter in s
-        for i in range(len(s)):            
-            # Look for odd length palindromes
-            l, r = i, i
-            while l in range(len(s)) and r in range(len(s)) and s[l] == s[r]:
-                if (r - l + 1) > longestPalindromeLength:
-                    currentSubstring = s[l:r+1]
-                    longestPalindromeLength = r - l + 1
-                l -= 1
-                r += 1
+        longestPalindromicSubstring = ""
+        # O(n^2) time complexity
+        for i in range(len(s)):
+            # Find the odd length palindrome
+            left, right = i, i
+            while left in range(len(s)) and right in range(len(s)) and s[left] == s[right]:
+                if (right - left + 1) > len(longestPalindromicSubstring):
+                    longestPalindromicSubstring = s[left:right + 1]
+                left -= 1
+                right += 1 
             
-            # Look for even length palindromes
-            l, r = i, i + 1
-            while l in range(len(s)) and r in range(len(s)) and s[l] == s[r]:
-                if (r - l + 1) > longestPalindromeLength:
-                    currentSubstring = s[l:r+1]
-                    longestPalindromeLength = r - l + 1
-                l -= 1
-                r += 1
-    
-        return currentSubstring
+            # Find the even length palindrome
+            left, right = i, i + 1
+            while left in range(len(s)) and right in range(len(s)) and s[left] == s[right]:
+                if (right - left + 1) > len(longestPalindromicSubstring):
+                    longestPalindromicSubstring = s[left:right + 1]
+                left -= 1
+                right += 1 
+            
+        return longestPalindromicSubstring
+            
+        
