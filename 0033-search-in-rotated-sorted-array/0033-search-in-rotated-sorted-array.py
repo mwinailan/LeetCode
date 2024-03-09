f2adc76a5aa83bb9c3bdac53e26d1f9c4c25d1b5
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-03-09 01:02:36 -0800

    Time: 48 ms (24.47%), Space: 16.8 MB (95.05%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,29 +1,25 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Binary search soluition for O(nlogn) runtime
         left, right = 0, len(nums) - 1
-        
-        while(left <= right):
-            mid = (left + right) // 2
+        # Binary search
+        # O(logn) time complexity
+        while ( left <= right ):
+            middle = ( right + left )// 2
+            if nums[middle] == target:
+                return middle
             
-            if nums[mid] == target:
-                return mid
-            # Case 1: If mid is in the left sorted portion
-            elif nums[mid] >= nums[0]:
-                # Case 1.1:
-                if target > nums[mid] or target < nums[0]:
-                    left = mid + 1
-                # Case 1.2:
-                else: 
-                    right = mid - 1
-                
-            # Case 2: If mid is in the right sorted portion
-            else:
-                # Case 2.1:
-                if target < nums[mid] or target > nums[len(nums) - 1]:
-                    right = mid - 1
-                # Case 2.2:
+            # Case 1 Search left sorted array
+            if nums[middle] >= nums[0]:
+                if nums[middle] < target or target < nums[0]:
+                    left = middle + 1
                 else:
-                    left = mid + 1
-            
+                    right = middle - 1
+                
+            # Case 2 Search right sorted array
+            else:            
+                if nums[middle] > target or target > nums[-1]:
+                    right = middle - 1
+                else:
+                    left = middle + 1
+                    
         return -1
