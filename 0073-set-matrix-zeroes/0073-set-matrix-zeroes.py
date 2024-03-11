6807ad7b7e4bc9b557c4147144b39a7e99310a55
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-03-10 20:23:13 -0700

    Time: 119 ms (6.78%), Space: 17.5 MB (38.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,27 +3,34 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rowZeroCheck = False
         ROWS, COLS = len(matrix), len(matrix[0])
+        topRow = False # Does top row have zero?
         
+        # Iterate through every element in matrix and find if we need to set zeroes
         for r in range(ROWS):
             for c in range(COLS):
                 if matrix[r][c] == 0:
                     matrix[0][c] = 0
-                    if r == 0:
-                        rowZeroCheck = True
-                    else:
+                    
+                    if r > 0:
                         matrix[r][0] = 0
-                        
+                    else:
+                        topRow = True
+        
+        # Set zeroes for the matrix other than the first collumn and row
         for r in range(1, ROWS):
             for c in range(1, COLS):
                 if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
         
+        # Set zeroes for the first column
         if matrix[0][0] == 0:
             for r in range(ROWS):
                 matrix[r][0] = 0
-                
-        if rowZeroCheck:
+        
+        # Set zeroes for the first row
+        if topRow:
             for c in range(COLS):
                 matrix[0][c] = 0
+            
+                    
