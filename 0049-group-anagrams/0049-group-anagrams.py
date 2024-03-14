5f3453f693f955f1b200e3ea79cd72d92112e6d8
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-03-13 23:31:01 -0700

    Time: 101 ms (20.26%), Space: 22.4 MB (6.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = defaultdict(list)
-        
+        groupAnagrams = defaultdict(list)
         for string in strs:
-            letterCount = [0] * 26
-            # Count the letters, and use the resulting list as a tuple for the key in hashmap
-            for s in string:
-                letterCount[ord(s) - ord("a")] += 1
+            count = [0] * 26
+            for c in string:
+                count[ord(c) - ord("a")] += 1
             
-            anagrams[tuple(letterCount)].append(string)
-                
+            groupAnagrams[tuple(count)].append(string)
         
-        return anagrams.values()
+        return groupAnagrams.values()
+            
         
