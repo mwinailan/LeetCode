6bfa4e9c7fa7fde3d3a67359211cb788107f0333
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-03-16 13:29:28 -0700

    Time: 52 ms (72.95%), Space: 16.6 MB (63.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,16 +1,22 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
-        # 2 pointer solution to find substring window
         left = 0
-        longestSubstring = 0
         isContained = set()
+        longestSubstringLength = 0
+        
+        # O(n) solution 
         for right in range(len(s)):
+            # while the letter is in the set, we remove it
             while s[right] in isContained:
                 isContained.remove(s[left])
                 left += 1
+            
+            # Add current letter to the set
             isContained.add(s[right])
-            longestSubstring = max(longestSubstring, right - left + 1)
+            
+            # calculate current length and compare to the longest
+            longestSubstringLength = max(longestSubstringLength, right - left + 1)
         
-        return longestSubstring
+        
+        return longestSubstringLength
         
