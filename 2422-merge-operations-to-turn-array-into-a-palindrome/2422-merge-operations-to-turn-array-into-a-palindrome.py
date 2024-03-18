dfacd97d6a1e8a82db0c82dc721e26eacd9cdc77
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-03-18 15:45:21 -0700

    Time: 704 ms (24.28%), Space: 32.2 MB (35.44%) - LeetHub

--- 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
+++ 2422-merge-operations-to-turn-array-into-a-palindrome/2422-merge-operations-to-turn-array-into-a-palindrome.py
@@ -3,19 +3,21 @@ class Solution:
         operations = 0
         left, right = 0, len(nums) - 1
         
-        4, 3, 2, 3, 4
+        #O(n) 2 pointer solution
+        #O(1) memory usage
         while(left < right):
-            if nums[left] > nums[right]:
-                nums[right] += nums[right - 1]
-                del nums[right - 1]
-                operations += 1
-            elif nums[left] < nums[right]:
-                nums[left] += nums[left + 1]
-                del nums[left + 1]
-                operations += 1
-            else:
+            if nums[left] == nums[right]:
                 left += 1
-            right -= 1
+                right -= 1
+            else:
+                if nums[left] + nums[left + 1] < nums[right] + nums[right - 1]:
+                    nums[left + 1] += nums[left]
+                    left += 1
+                else:
+                    nums[right - 1] += nums[right]
+                    right -= 1
+                operations += 1
+                    
         
         return operations
         
