ca64f428d63d225a61c271134ee71537492965fb
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-04-09 23:58:35 -0700

    Time: 172 ms (33.17%), Space: 18.1 MB (35.15%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         res, count = 0, 0
+        #linear solution
         for n in nums:
-            if count == 0:
-                res = n
+            if n == res:
+                count += 1
+            else:
+                count -= 1
             
-            count += 1 if res == n else -1
+            if count < 0:
+                count = 0
+                res = n
         
         return res
