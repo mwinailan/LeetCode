602a7ab2090cda1d886bab76925edc93c9fa3abc
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-04-15 21:10:49 -0700

    Time: 227 ms (29.22%), Space: 18.1 MB (82.22%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,35 +1,40 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        flowsToPacific, flowsToAtlantic = set(), set()
+        flows_to_atlantic = set()
+        flows_to_pacific =  set()
         
-        # DFS solution to mark (r,c)
-        def markLand(r, c, flowsTo, prevHeight):
-            if r not in range(ROWS) or c not in range(COLS) or (r,c) in flowsTo or heights[r][c] < prevHeight:
+        # O(n) time, O(n) space complexity
+        # DFS Solution
+        def mark_grid(r, c, flows_to_ocean, prev_height):
+            if r not in range(ROWS) or c not in range(COLS) or (r,c) in flows_to_ocean or prev_height > heights[r][c]:
                 return
-            flowsTo.add((r,c))
             
-            markLand(r + 1, c, flowsTo, heights[r][c])
-            markLand(r - 1, c, flowsTo, heights[r][c])
-            markLand(r, c + 1, flowsTo, heights[r][c])
-            markLand(r, c - 1, flowsTo, heights[r][c])
+            flows_to_ocean.add((r,c))
             
-            
+            directions = [[1,0], [-1,0], [0,1], [0,-1]]
+            for dx, dy in directions:
+                mark_grid(r + dx, c + dy, flows_to_ocean, heights[r][c])
         
-        # Iterate through rows
+        # mark grid starting from the edges
         for r in range(ROWS):
-            markLand(r, 0, flowsToPacific, heights[r][0])
-            markLand(r, COLS - 1, flowsToAtlantic, heights[r][COLS - 1])
-        # Iterate through cols
+            mark_grid(r, 0, flows_to_pacific, heights[r][0])
+            mark_grid(r, COLS - 1, flows_to_atlantic, heights[r][COLS - 1])
+            
         for c in range(COLS):
-            markLand(0, c, flowsToPacific, heights[0][c])
-            markLand(ROWS - 1, c, flowsToAtlantic, heights[ROWS - 1][c])
+            mark_grid(0, c, flows_to_pacific, heights[0][c])
+            mark_grid(ROWS - 1, c, flows_to_atlantic, heights[ROWS - 1][c])
         
-        result = []
-        #Iterate through every (r,c) and add to res if both in atlantic and pacific
+        res = []
         for r in range(ROWS):
             for c in range(COLS):
-                if (r,c) in flowsToPacific and (r,c) in flowsToAtlantic:
-                    result.append([r,c])
+                if (r,c) in flows_to_pacific and (r,c) in flows_to_atlantic:
+                    res.append([r,c])
         
-        return result
+        return res
+                    
+                
+        
+        
+        
+        
