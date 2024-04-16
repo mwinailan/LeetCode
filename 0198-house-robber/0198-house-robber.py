79d126feddfe5faab9792868326dc173115af4bf
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-04-15 22:54:52 -0700

    Time: 29 ms (92.33%), Space: 16.5 MB (89.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,11 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # house1   house2  curhouse
-        house1, house2 = 0, 0
-        # DP solution
-        for n in nums:
-            curhouse = max(n + house1, house2)
-            house1 = house2
-            house2 = curhouse
+        #house_a house_b current_house
+        house_a = 0
+        house_b = 0
+        max_profit = 0
+        for i in range(len(nums)):
+            current_house = max(nums[i] + house_a, house_b, nums[i])
+            max_profit = max(max_profit, current_house)
+            house_a = house_b
+            house_b = current_house
         
-        return house2
+        return max_profit
+        
