385087ecd1cb9dd489bbfe4e7a5697d7847fccc0
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-04-15 22:40:13 -0700

    Time: 39 ms (65.10%), Space: 17.5 MB (84.40%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -10,5 +10,22 @@ class Solution:
             return 0
         
         
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        max_depth = 0
+        # iterative bfs
+        queue = collections.deque()
+        queue.append(root)
         
+        while queue:
+            queue_length = len(queue)
+            for _ in range(queue_length):
+                current_node = queue.popleft()
+                
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+            
+            max_depth += 1
+                
+        
+        return max_depth
