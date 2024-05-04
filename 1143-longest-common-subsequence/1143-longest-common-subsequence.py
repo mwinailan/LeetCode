bf7d3b95ae26747876e4060158105b943200c1e8
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-05-03 23:43:19 -0700

    Time: 484 ms (60.10%), Space: 42.4 MB (42.68%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,16 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # Create a 2d matrix
-        lcsMatrix = [[0 for i in range(len(text1) + 1)] for j in range(len(text2) + 1)]
+        LIS = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
         
-        # Start bottoms up approach
-        for i in range(len(text2) - 1, -1, -1):
-            for j in range(len(text1) - 1, -1, -1):
-                #Case 1: text1 and text2 are the same
-                if text2[i] == text1[j]:
-                    lcsMatrix[i][j] = 1 + lcsMatrix[i+1][j+1]
-                # Case 2:
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    LIS[i][j] = 1 + LIS[i + 1][j + 1]
                 else:
-                    lcsMatrix[i][j] = max(lcsMatrix[i+1][j], lcsMatrix[i][j+1])
+                    LIS[i][j] = max(LIS[i+1][j], LIS[i][j+1])
         
-        return lcsMatrix[0][0]
+        return LIS[0][0]
