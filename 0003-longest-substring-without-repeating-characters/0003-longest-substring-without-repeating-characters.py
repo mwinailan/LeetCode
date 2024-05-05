6966cc944d36b1f33d846027431c360361745c0f
Matthew Winailan <60530158+mwinailan@users.noreply.github.com>
2024-05-04 23:43:28 -0700

    Time: 55 ms (59.60%), Space: 16.7 MB (19.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,22 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        left = 0
-        isContained = set()
-        longestSubstringLength = 0
+        contained_letters = set()
+        longest_substring_without_repeating = 0
         
-        # O(n) solution 
+        left = 0
         for right in range(len(s)):
-            # while the letter is in the set, we remove it
-            while s[right] in isContained:
-                isContained.remove(s[left])
+            while s[right] in contained_letters:
+                contained_letters.remove(s[left])
                 left += 1
             
-            # Add current letter to the set
-            isContained.add(s[right])
+            contained_letters.add(s[right])
+            longest_substring_without_repeating = max(longest_substring_without_repeating, right - left + 1)
             
-            # calculate current length and compare to the longest
-            longestSubstringLength = max(longestSubstringLength, right - left + 1)
-        
-        
-        return longestSubstringLength
-        
+        return longest_substring_without_repeating
